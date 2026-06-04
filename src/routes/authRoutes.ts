05a9bb1a1e0234b6e3190156b1a9874c83c8fa30
Gaby cocuy <gcocuymarin@gmail.com>
2026-06-04 00:07:58 -0500

    refactor: remove duplicate backend and secure environment files

--- src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerHost, loginHost } from "../controllers/authController";
-
-const router = express.Router();
-
-router.post("/register", registerHost);
-router.post("/login", loginHost);
-
-export default router;
--- src/routes/voteRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { createVote } from "../controllers/voteController";
-
-const router = express.Router();
-
-router.post("/", createVote);
-
-export default router;
